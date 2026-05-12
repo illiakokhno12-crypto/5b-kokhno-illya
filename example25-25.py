34a38237d15e1a800bedc0614d90a25297b87d22
illiakokhno12-crypto <illiakokhno12@gmail.com>
2026-05-12 19:14:31 +0000

    Вправа 25 параграф 25

---
+++ example25-25.py
@@ -0,0 +1,24 @@
+from turtle import *
+Screen().setup(1.0,1.0)
+delay(100)
+fillcolor("brown")
+begin_fill()
+goto(-40,50)
+goto(120,50)
+goto(80,0)
+goto(0,0)
+end_fill()
+color("orange", "orange")
+up()
+goto(200,200)
+down()
+begin_fill()
+circle(30)
+end_fill()
+
+
+
+
+
+
+done()
